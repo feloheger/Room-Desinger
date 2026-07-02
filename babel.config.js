b705ee2da45c23d99f2eecf82ab9addd6327c7c5
feloheger <feloheger@gmail.com>
2026-07-02 11:37:52 +0200

    Add files via upload

--- babel.config.js
+++ babel.config.js
@@ -3,12 +3,8 @@ module.exports = function (api) {
   return {
     presets: [
       ["babel-preset-expo", { jsxImportSource: "nativewind" }],
-      "nativewind/babel",
     ],
     plugins: [
-      // expo-router requires this plugin
-      "expo-router/babel",
-      // Reanimated plugin MUST be listed last
       "react-native-reanimated/plugin",
     ],
   };
